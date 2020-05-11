d40db1229eed436268bcc7dafaa4c621727117d2
Haxos <a.mayocartes@protonmail.com>
2020-05-11 21:26:55 +0200

    added base json return in express server (financial transactions because why not)

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,9 @@
 import Express from 'express';
-import Chance from 'chance';
-var chance = new Chance();
+import Utils from './utils.js';
 
 const app = Express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send(Utils.generateTransaction()))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
