39da61a053f9691824c5dc33a8b403e5f2a01ed0
Haxos <a.mayocartes@protonmail.com>
2020-05-12 14:27:24 +0200

    added base ajax request

--- public/vue/home.php
+++ public/vue/home.php
@@ -7,5 +7,14 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <div class="displayer">
+    </div>
 </body>
 </html>
+
+<script
+    src="https://code.jquery.com/jquery-3.5.1.min.js"
+    integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+    crossorigin="anonymous"
+></script>
+<script src="js/transactions.js"></script>
