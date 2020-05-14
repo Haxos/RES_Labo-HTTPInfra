dd1fbc90784d0fdefc18e9a52eb216604f4d8507
Fl0Cri <florent@inetis.ch>
2020-05-14 21:44:12 +0200

    Basic template

---
+++ public/partials/footer.php
@@ -0,0 +1,5 @@
+<footer id="footer" class="mt-5">
+    <div class="container text-center">
+        &copy; <?= date('Y') ?> Adrian Mayo Cartes & Florent Crisinel
+    </div>
+</footer>
---
+++ public/partials/head.php
@@ -0,0 +1,6 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title><?= $pageTitle ?></title>
+<link href="/assets/css/bootstrap.min.css" rel="stylesheet" />
+<link href="/assets/css/custom.css" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css2?family=Martel:wght@300&family=Montserrat&display=swap" rel="stylesheet">
